ed2249642f64703aafb0cd6e3c5698625065c551
AniruddhSin <146397992+AniruddhSin@users.noreply.github.com>
2024-11-04 13:05:54 -0800

    small change for L16

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 // todo
+import "./style.css";
+const _app: HTMLDivElement = document.querySelector("#app")!;
+
+const tempButton = document.createElement("button");
+tempButton.innerText = "Click Me!";
+tempButton.addEventListener("click", () => {
+  console.log("the button was clicked!");
+});
+_app.append(tempButton);
