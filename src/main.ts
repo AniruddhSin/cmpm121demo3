d8d408a1a782f698724ae17615e91f2a3d0d9f69
AniruddhSin <146397992+AniruddhSin@users.noreply.github.com>
2024-11-20 21:41:10 -0800

    D3.C finished

--- src/main.ts
+++ src/main.ts
@@ -20,13 +20,15 @@ interface Coin {
 }
 interface Cache {
   coins: Coin[];
+  toMemento(): string;
 }
-const allCaches: Map<GridPoint, Cache> = new Map();
+// first string is stringified GridPoint
+const availableCaches: Map<string, Cache> = new Map();
+const mementoCaches: Map<string, string> = new Map();
 
 // Set up Event Bus to handle dispatched Events
 const bus = new EventTarget();
 /* Possible Event Names:
-player_moved
 inventory_changed
 */
 function notifyBus(eventName: string) {
@@ -38,10 +40,11 @@ const title = document.getElementById("title")!;
 title.innerText = "NFT Colletion";
 
 // Tunable gameplay parameters
-const SPAWN_POINT = leaflet.latLng(0, 0);
+const SPAWN_POINT = leaflet.latLng(36.98949379578401, -122.06277128548504);
+//const SPAWN_POINT = leaflet.latLng(0,0);
 const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
-const NEIGHBORHOOD_SIZE = 8;
+const NEIGHBORHOOD_SIZE = 5;
 const CACHE_SPAWN_PROBABILITY = 0.1;
 
 // Create the map (element with id "map" is defined in index.html)
@@ -63,9 +66,14 @@ leaflet
   .addTo(map);
 
 // Add a marker to represent the player
-const playerMarker = leaflet.marker(SPAWN_POINT);
+const playerLocation = SPAWN_POINT;
+const playerMarker = leaflet.marker(playerLocation);
 playerMarker.bindTooltip("That's you!");
 playerMarker.addTo(map);
+// use curr lat/lng to get grid location
+
+// Set up rectangle group to help redrawing
+const rectGroup = leaflet.layerGroup().addTo(map);
 
 // Set up NFT coins
 const collectedCoins: Coin[] = [];
@@ -86,7 +94,7 @@ function updateInventory() {
 // Set up collection and depositing of coins
 // collection/depositing of coins will be randomized to add a twist to the nature of the game
 function collect(cell: GridPoint) {
-  const collectCache = allCaches.get(cell);
+  const collectCache = availableCaches.get(JSON.stringify(cell));
   if (collectCache && collectCache.coins.length > 0) {
     const whichCoin: number = Math.floor(
       Math.random() * collectCache.coins.length,
@@ -97,7 +105,7 @@ function collect(cell: GridPoint) {
   }
 }
 function deposit(cell: GridPoint) {
-  const depositCache = allCaches.get(cell);
+  const depositCache = availableCaches.get(JSON.stringify(cell));
   if (depositCache && collectedCoins.length > 0) {
     const whichCoin: number = Math.floor(Math.random() * collectedCoins.length);
     const coin: Coin = collectedCoins.splice(whichCoin, 1).pop()!;
@@ -106,38 +114,67 @@ function deposit(cell: GridPoint) {
   }
 }
 
-// Add caches to a given point on the map
+// Add caches to a given point on the map ; point is a global point, not grid location
 function spawnCache(point: GridPoint) {
   // Convert cell numbers into lat/lng bounds
   const bounds = leaflet.latLngBounds([
     [
-      SPAWN_POINT.lat + point.i * TILE_DEGREES,
-      SPAWN_POINT.lng + point.j * TILE_DEGREES,
+      point.i * TILE_DEGREES,
+      point.j * TILE_DEGREES,
     ],
     [
-      SPAWN_POINT.lat + (point.i + 1) * TILE_DEGREES,
-      SPAWN_POINT.lng + (point.j + 1) * TILE_DEGREES,
+      (point.i + 1) * TILE_DEGREES,
+      (point.j + 1) * TILE_DEGREES,
     ],
   ]);
   // Add a rectangle to the map to represent the cache
   const rect = leaflet.rectangle(bounds);
-  rect.addTo(map);
+  rect.addTo(rectGroup);
 
-  const cache: Cache = {
-    coins: [],
-  };
-  // Populate the cache
-  for (
-    let i = 0;
-    i < Math.floor(luck([point.i, point.j, "initialValue"].toString()) * 8);
-    i++
+  // create new cache if not in mementoCaches
+  // if in memento caches, take it away from memento
+  let cache: Cache;
+  if (
+    mementoCaches.has(
+      JSON.stringify(point),
+    )
   ) {
-    cache.coins.push({
-      cell: point,
-      serial: i.toString(),
-    });
+    cache = {
+      coins: JSON.parse(
+        mementoCaches.get(
+          JSON.stringify(point),
+        )!,
+      ),
+      toMemento() {
+        return JSON.stringify(this.coins);
+      },
+    };
+    mementoCaches.delete(
+      JSON.stringify(point),
+    );
+  } else {
+    cache = {
+      coins: [],
+      toMemento() {
+        return JSON.stringify(this.coins);
+      },
+    };
+    // Populate the cache
+    for (
+      let i = 0;
+      i < Math.floor(luck([point.i, point.j, "initialValue"].toString()) * 8);
+      i++
+    ) {
+      cache.coins.push({
+        cell: point,
+        serial: i.toString(),
+      });
+    }
   }
-  allCaches.set(point, cache);
+  availableCaches.set(
+    JSON.stringify(point),
+    cache,
+  );
   // Handle interactions with the cache
   rect.bindPopup(() => {
     // The popup offers a description and button
@@ -167,12 +204,65 @@ function spawnCache(point: GridPoint) {
   });
 }
 
-// Look around the player's neighborhood for caches to spawn
-for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-    // If location i,j is lucky enough, spawn a cache!
-    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
-      spawnCache({ i, j });
+// Set up movement keys
+// While this could have been done using a Command pattern, future updates will not require more usability
+// so it is being hard coded
+function movePlayer(lat: number, lng: number) {
+  const newLat = playerLocation.lat + lat * TILE_DEGREES;
+  const newLng = playerLocation.lng + lng * TILE_DEGREES;
+  playerLocation.lat = newLat;
+  playerLocation.lng = newLng;
+  playerMarker.setLatLng(playerLocation);
+  map.panTo(playerLocation);
+  updateLocation();
+}
+const northButton = document.getElementById("north");
+northButton!.addEventListener("click", () => {
+  movePlayer(1, 0);
+});
+const eastButton = document.getElementById("east");
+eastButton!.addEventListener("click", () => {
+  movePlayer(0, 1);
+});
+const southButton = document.getElementById("south");
+southButton!.addEventListener("click", () => {
+  movePlayer(-1, 0);
+});
+const westButton = document.getElementById("west");
+westButton!.addEventListener("click", () => {
+  movePlayer(0, -1);
+});
+
+function updateLocation() {
+  // memento all the existing caches (if there are any) and delete available caches
+  availableCaches.forEach((value, key) => {
+    mementoCaches.set(key, value.toMemento());
+    availableCaches.delete(key);
+  });
+  rectGroup.clearLayers();
+  // spawn caches but if there is a memento of one, reify it
+  const offset = {
+    i: Math.floor(playerLocation.lat / TILE_DEGREES),
+    j: Math.floor(playerLocation.lng / TILE_DEGREES),
+  };
+  for (
+    let i = offset.i - NEIGHBORHOOD_SIZE;
+    i < offset.i + NEIGHBORHOOD_SIZE;
+    i++
+  ) {
+    for (
+      let j = offset.j - NEIGHBORHOOD_SIZE;
+      j < offset.j + NEIGHBORHOOD_SIZE;
+      j++
+    ) {
+      // If location i,j is lucky enough, spawn a cache!
+      if (
+        luck([i, j].toString()) <
+          CACHE_SPAWN_PROBABILITY
+      ) {
+        spawnCache({ i, j });
+      }
     }
   }
 }
+updateLocation();
